2c8fe421c498c1b6267ab1c658bec310ac0b171f
BelikanM <nyundumathryme@gmail.com>
2025-04-04 10:07:04 +0100

    Mise à jour des fichiers

---
+++ src/components/SplashScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SplashScreen.css'; // Pour les styles
+
+function SplashScreen() {
+  return (
+    <div className="splash-screen">
+      <img src="/logo192.png" alt="Logo" className="logo" />
+    </div>
+  );
+}
+
+export default SplashScreen;
