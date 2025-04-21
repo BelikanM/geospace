ff13873e937e84357d8307f1dab22eb43baf85f3
BelikanM <nyundumathryme@gmail.com>
2025-04-21 22:18:43 +0100

    Mise à jour des fichiers

--- src/pages/CHAT.js
+++ src/pages/CHAT.js
@@ -154,7 +154,7 @@ const Audio = () => {
     formData.append('file', audioFile);
 
     try {
-      const response = await axios.post('http://0.0.0.0:5400/api/analyze', formData, {
+      const response = await axios.post('http://127.0.0.1:5400/api/analyze', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         },
